76e341c97fceff624f9a226b94f5fd96be1848a0
trashb <ashleybi2005@gmail.com>
2024-10-21 10:45:22 -0400

    implemented main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -19,6 +19,7 @@ public class Main {
                                             .addLoggedInView()
                                             .addSignupUseCase()
                                             .addLoginUseCase()
+                                            .addLogoutUseCase()
                                             .addChangePasswordUseCase()
                                             .build();
 
